a0be3598e25ae0b5cee1086e515b999c5d15dd53
alekseikolesnikov <kolesnikov.aleksei.04@gmail.com>
2023-07-19 18:26:47 -0600

    background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import '../styles/header.css';
 
 export function Header() {
     return (
         <header className={"header"}>
-            <div className={"header_logo_container"}></div>
-            <div className={"header_list_container"}>
-
-            </div>
-            <div className={"header_input_container"}>
-                <input className={"header_input"}/>
-                <div></div>
+            <div className={"header__container _container"}>
+                <a href={""} className={"header__logo"}>
+                    <span>MIRROR</span>
+                    <span>movie</span>
+                </a>
+                <nav className={"header__menu menu"}>
+                    <ul className={"menu__list"}>
+                        <li className={"menu__item"}>
+                            <a href={""} className={"menu__link"}>Фильмы</a>
+                        </li>
+                        <li className={"menu__item"}>
+                            <a href={""} className={"menu__link"}>Сериалы</a>
+                        </li>
+                        <li className={"menu__item"}>
+                            <a href={""} className={"menu__link"}>Мультфилмы</a>
+                        </li>
+                        <li className={"menu__item"}>
+                            <a href={""} className={"menu__link"}>Катигории</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={"header__search"}>
+                    <input className={"header_input"}/>
+                    <div className={"menu__item"}>
+                        <a href={""} className={"menu__link"}>Войти</a>
+                    </div>
+                </div>
             </div>
         </header>
     );
